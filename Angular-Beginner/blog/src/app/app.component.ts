88931b4f28596f993615a3c2fc0b27a7b5f20e9d
Sufiyan33 <basu700736@gmail.com>
2024-10-13 11:13:53 +0530

    Created inline-style component

--- Angular-Beginner/blog/src/app/app.component.ts
+++ Angular-Beginner/blog/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from "./user-list/user-list.component";
+import { MyCompComponent } from "./my-comp/my-comp.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent],
+  imports: [RouterOutlet, UserListComponent, MyCompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Angular-Beginner/blog/src/app/my-comp/my-comp.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-comp.component.html',
+  styles: `
+    .custom{color: red}
+  `
+})
+export class MyCompComponent {
+
+}
