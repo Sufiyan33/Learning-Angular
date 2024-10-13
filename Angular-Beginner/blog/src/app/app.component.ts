95d1bcc96308ddaf52307b0cee559034cf6815c1
Sufiyan33 <basu700736@gmail.com>
2024-10-13 11:26:08 +0530

    Created inline-template component

--- Angular-Beginner/blog/src/app/app.component.ts
+++ Angular-Beginner/blog/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from "./user-list/user-list.component";
 import { MyCompComponent } from "./my-comp/my-comp.component";
+import { EmployeeComponent } from './employee/employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent, MyCompComponent],
+  imports: [RouterOutlet, UserListComponent, MyCompComponent, EmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
