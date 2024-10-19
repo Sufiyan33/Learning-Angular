fbfd582685bb29f2e99c833fead41131c78c2edd
Sufiyan33 <basu700736@gmail.com>
2024-10-19 16:01:59 +0530

    Printing list of username in ui

--- Angular-Beginner/blog/src/app/user-list/user-list.component.ts
+++ Angular-Beginner/blog/src/app/user-list/user-list.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+export interface Item{
+  id: number;
+  name: String;
+}
 @Component({
   selector: 'app-user-list',
   standalone: true,
@@ -9,4 +13,15 @@ import { Component } from '@angular/core';
 })
 export class UserListComponent {
 
+  items: Item[] =[
+    {id: 1, name:"user1"},
+    {id: 2, name: "user2"},
+    {id: 3, name: "user3"},
+  ];
+  
+  selectedItem:any;
+  selectItem(item:any){
+    this.selectedItem= item;
+    alert('item id: ${item.id}, item name: ${item.name}');
+  }
 }
