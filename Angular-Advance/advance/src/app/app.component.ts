7b0bf8da977fb7a2034e0851c338ff53ad3a44d9
Sufiyan33 <basu700736@gmail.com>
2024-10-24 15:08:43 +0530

    Registering directive in main component

--- Angular-Advance/advance/src/app/app.component.ts
+++ Angular-Advance/advance/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RedElementDirective } from './red-element.directive'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RedElementDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Angular Advance Topics';
+  directiveTitle = 'Hello!!! Welcome to directive.';
 }
