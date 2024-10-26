1b94e5eb561441d7883ba18aa563176f1251e5a4
Sufiyan33 <basu700736@gmail.com>
2024-10-26 09:43:15 +0530

    applied validation on name and password fields

--- Angular-Advance/advance/src/app/app.component.ts
+++ Angular-Advance/advance/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RedElementDirective } from './red-element.directive'; 
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RedElementDirective, FormsModule],
+  imports: [RouterOutlet, RedElementDirective, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
