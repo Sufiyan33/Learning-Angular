7a2cc601d8747888fb7eca2bb1b98c8bb685eb27
Sufiyan33 <basu700736@gmail.com>
2024-10-29 23:00:25 +0530

    Created route object

--- Angular-Advance/advance/src/app/app.routes.ts
+++ Angular-Advance/advance/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'user',
+        component: UserComponent
+    },
+    {
+        path: 'user-list',
+        component: UserListComponent
+    }
+];
