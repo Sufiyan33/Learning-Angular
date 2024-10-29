47ed75a809d484dfd2df367aec3b380102a51cf4
Sufiyan33 <basu700736@gmail.com>
2024-10-29 23:39:23 +0530

    Adding html redirecting

--- Angular-Advance/advance/src/app/user-list/user-list.component.ts
+++ Angular-Advance/advance/src/app/user-list/user-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
