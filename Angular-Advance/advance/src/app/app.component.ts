2cc59b2202b01e6ef04bd20f5d10bb0079c8d154
Sufiyan33 <basu700736@gmail.com>
2024-10-29 11:31:34 +0530

    Example of pipes

--- Angular-Advance/advance/src/app/app.component.ts
+++ Angular-Advance/advance/src/app/app.component.ts
@@ -16,6 +16,7 @@ import { FormControl, FormGroup, Validator } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Angular Advance Topics';
+  today = Date();
   directiveTitle = 'Hello!!! Welcome to directive.';
 
   userLogin(item:any){
