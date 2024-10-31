174c640e785fe0a4ea2648459c99837d105f3c14
Sufiyan33 <basu700736@gmail.com>
2024-10-31 15:04:42 +0530

    Created userdata into service

--- Angular-Advance/advance/src/app/services/user-data.service.ts
+++ Angular-Advance/advance/src/app/services/user-data.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class UserDataService {
 
   constructor() { }
+  users(){
+    return [
+      {name: 'Sufiyan', age: 26, email: 'abc@gmailc.com'},
+      {name: 'Sabir', age: 27, email: 'abcd@gmail.com'},
+      {name: 'Alfaz', age: 28, email: 'abcde@gmail.com'},
+      {name: 'Abhisekt', age: 29, email: 'abcdef@gmail.com'},
+    ]
+  }
 }
