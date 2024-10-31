92801528f9aef3846480b656b4b558b12c3c5d9b
Sufiyan33 <basu700736@gmail.com>
2024-10-31 15:44:22 +0530

    Fetching service class data into footer component

--- Angular-Advance/advance/src/app/footer/footer.component.ts
+++ Angular-Advance/advance/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserDataService } from '../services/user-data.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  dataForUser: any[]=[];
+  constructor(private usersData: UserDataService){
+    console.warn("User Data:: ", usersData.usersAllData())
+    this.dataForUser=usersData.usersAllData();
+  }
 }
