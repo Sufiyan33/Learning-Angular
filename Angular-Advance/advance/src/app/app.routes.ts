6ecd037c1675dac7612052e6a7ca2abc5f026561
Sufiyan33 <basu700736@gmail.com>
2024-11-02 12:11:38 +0530

    Adding routing in header

--- Angular-Advance/advance/src/app/app.routes.ts
+++ Angular-Advance/advance/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserListComponent } from './user-list/user-list.component';
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 import { AboutUserComponent } from './about-user/about-user.component';
 import { AboutuserlistComponent } from './aboutuserlist/aboutuserlist.component';
+import { ApiintegrationComponent } from './api-integration/apiintegration.component';
 
 export const routes: Routes = [
     {
@@ -17,6 +18,10 @@ export const routes: Routes = [
             {path: 'about-user', component: AboutUserComponent},
             {path: 'aboutUser-list', component: AboutuserlistComponent}
         ]
+    },
+    {
+        path: 'get-api',
+        component: ApiintegrationComponent
     },
     {
         path: '**',
