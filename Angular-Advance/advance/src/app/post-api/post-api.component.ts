76baf99d27c23c12fcc8cdeb2053011eee5206a1
Sufiyan33 <basu700736@gmail.com>
2024-11-02 14:55:01 +0530

    Post api data and fetching same api data and displaying

--- Angular-Advance/advance/src/app/post-api/post-api.component.ts
+++ Angular-Advance/advance/src/app/post-api/post-api.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-api',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-api.component.html',
   styleUrl: './post-api.component.scss'
 })
-export class PostApiComponent {
+export class PostApiComponent implements OnInit{
+  userObj: any = {
+    "userId": 2,
+    "id": 0,
+    "title": "",
+    "body": ""
+  }
 
+  constructor(private http: HttpClient){}
+  url = "https://jsonplaceholder.typicode.com/posts";
+
+  userDataFromPost: any[]=[];
+  flag = false;
+
+  ngOnInit(): void {
+    this.getAllDataFromPost();
+  }
+  onSave(){
+    this.http.post(this.url, this.userObj).subscribe((result:any) =>{
+      this.getAllDataFromPost();
+      console.warn(result)
+    })
+  }
+
+  getAllDataFromPost(){
+    this.http.get("https://jsonplaceholder.typicode.com/posts/").subscribe((res:any) =>{
+      this.userDataFromPost = res;
+    })
+  }
 }
