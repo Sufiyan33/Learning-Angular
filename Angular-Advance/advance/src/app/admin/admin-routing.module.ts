a5f2ac6303965a3a065a1168a307c872c53473b6
Sufiyan33 <basu700736@gmail.com>
2024-11-05 12:55:35 +0530

    Added routes for admin components

--- Angular-Advance/advance/src/app/admin/admin-routing.module.ts
+++ Angular-Advance/advance/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NormalUserComponent } from './normal-user/normal-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'nUser',
+    component: NormalUserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
