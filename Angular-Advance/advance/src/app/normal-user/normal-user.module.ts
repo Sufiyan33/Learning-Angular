23cbdf5fa888618da1f7896737666dfe025aeee1
Sufiyan33 <basu700736@gmail.com>
2024-11-05 12:48:38 +0530

    Created second module

---
+++ Angular-Advance/advance/src/app/normal-user/normal-user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NormalUserRoutingModule } from './normal-user-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NormalUserRoutingModule
+  ]
+})
+export class NormalUserModule { }
