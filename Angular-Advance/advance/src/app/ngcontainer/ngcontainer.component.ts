3109f3bf246c0d356bd3924d2e9e5a11ac9295eb
Sufiyan33 <basu700736@gmail.com>
2024-11-06 11:13:15 +0530

    Created component for showing ngcontainer use

---
+++ Angular-Advance/advance/src/app/ngcontainer/ngcontainer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngcontainer',
+  standalone: true,
+  imports: [],
+  templateUrl: './ngcontainer.component.html',
+  styleUrl: './ngcontainer.component.scss'
+})
+export class NgcontainerComponent {
+
+}
