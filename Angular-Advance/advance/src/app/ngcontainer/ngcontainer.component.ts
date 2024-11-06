3e06c3a81747f6885904a7676ae0eb934477603f
Sufiyan33 <basu700736@gmail.com>
2024-11-06 12:23:32 +0530

    Displaying text using ngcontainer

--- Angular-Advance/advance/src/app/ngcontainer/ngcontainer.component.ts
+++ Angular-Advance/advance/src/app/ngcontainer/ngcontainer.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ngcontainer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ngcontainer.component.html',
   styleUrl: './ngcontainer.component.scss'
 })
 export class NgcontainerComponent {
-
+  // ng-conainer or text inside it will be display if isContainer will be true
+  isContainer: boolean = true;
 }
