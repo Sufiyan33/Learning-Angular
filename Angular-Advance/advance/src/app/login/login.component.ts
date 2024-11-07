7c6b9ff3f9515321ed10e8ef0df162a1af235658
Sufiyan33 <basu700736@gmail.com>
2024-11-07 23:26:50 +0530

    Added login page functionality

--- Angular-Advance/advance/src/app/login/login.component.ts
+++ Angular-Advance/advance/src/app/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  userObj:any = {
+    userName: '',
+    password: ''
+  }
+
+  constructor(private router:Router){}
+  onLogin(){
+    if(this.userObj.userName == "admin" && this.userObj.password == "123"){
+      alert('Login successfully');
+      // before redirecting to the page store credential into local storage
+      localStorage.setItem('loginuser', this.userObj.userName)
+      this.router.navigateByUrl('user-list');
+    }else{
+      alert('Invalid credentials')
+    }
+  }
 }
