16eaecd9d7312fec2e6ac8a1564c58b42b043701
Sufiyan33 <basu700736@gmail.com>
2024-11-07 22:11:24 +0530

    Adding default routes

--- Angular-Advance/advance/src/app/app.routes.ts
+++ Angular-Advance/advance/src/app/app.routes.ts
@@ -6,43 +6,61 @@ import { AboutUserComponent } from './about-user/about-user.component';
 import { AboutuserlistComponent } from './aboutuserlist/aboutuserlist.component';
 import { ApiintegrationComponent } from './api-integration/apiintegration.component';
 import { PostApiComponent } from './post-api/post-api.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
+    // Creating default routes for admin login.
     {
-        path: 'user/:id',
-        component: UserComponent
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
     },
     {
-        path: 'user-list',
-        component: UserListComponent,
-        children:[
-            {path: 'about-user', component: AboutUserComponent},
-            {path: 'aboutUser-list', component: AboutuserlistComponent}
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'user/:id',
+                component: UserComponent
+            },
+            {
+                path: 'user-list',
+                component: UserListComponent,
+                children:[
+                    {path: 'about-user', component: AboutUserComponent},
+                    {path: 'aboutUser-list', component: AboutuserlistComponent}
+                ]
+            },
+            {
+                path: 'get-api',
+                component: ApiintegrationComponent
+            },
+            {
+                path: 'add-data',
+                component: PostApiComponent
+            },
+            {
+                path: 'put-api',
+                component: PostApiComponent
+            },
+            {
+                path: 'delete-api',
+                component: PostApiComponent
+            },
+            {
+                path: '**',
+                component: NoPageFoundComponent
+            },
+            // Now add here admin module routes so that in url it looks like admin/login or admin/user
+            {
+                path: 'admin',
+                loadChildren: ()=>import('./admin/admin.module').then(mod=>mod.AdminModule)
+            }
         ]
-    },
-    {
-        path: 'get-api',
-        component: ApiintegrationComponent
-    },
-    {
-        path: 'add-data',
-        component: PostApiComponent
-    },
-    {
-        path: 'put-api',
-        component: PostApiComponent
-    },
-    {
-        path: 'delete-api',
-        component: PostApiComponent
-    },
-    {
-        path: '**',
-        component: NoPageFoundComponent
-    },
-    // Now add here admin module routes so that in url it looks like admin/login or admin/user
-    {
-        path: 'admin',
-        loadChildren: ()=>import('./admin/admin.module').then(mod=>mod.AdminModule)
-    }
+    }, 
 ];
