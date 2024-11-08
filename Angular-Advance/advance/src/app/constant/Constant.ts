3a371a222121cefdbe1256b4fd074d8adbfb7795
Sufiyan33 <basu700736@gmail.com>
2024-11-08 22:49:28 +0530

    Created constant file

---
+++ Angular-Advance/advance/src/app/constant/Constant.ts
@@ -0,0 +1,14 @@
+export const Constant = {
+    POST_API_URL: 'https://jsonplaceholder.typicode.com/posts',
+    GET_API_URL: 'https://jsonplaceholder.typicode.com/users',
+    PUT_API_URL: 'https://jsonplaceholder.typicode.com/posts/2',
+    DELETE_API_URL: 'https://jsonplaceholder.typicode.com/posts/2',
+
+    VALIDATION_MESSAGES: {
+        NAME_REQUIRED: 'Name is required',
+        VALID_NAME: 'Please enter valid name',
+        PASSWORD_REQUIRED: 'Please enter password',
+        MIN_LENGTH: 'Password should be more than 2 digits',
+        MAX_LENGTH: 'Password should be max 8 digits'
+    }
+}
