46b34df6ea3438eb624727482ca88f2e6ca016d7
Sufiyan33 <basu700736@gmail.com>
2024-11-08 16:26:55 +0530

    Making api call for login

--- Angular-Advance/advance/src/app/login/login.component.ts
+++ Angular-Advance/advance/src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -11,13 +12,18 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  userObj:any = {
+ /* userObj:any = {
     userName: '',
     password: ''
+  }*/
+  userObj: any={
+    EmailId: '',
+    Pasword: ''
   }
 
-  constructor(private router:Router){}
-  onLogin(){
+  constructor(private router:Router, private http: HttpClient){}
+  // commenting this code and adding new code to make api call.
+ /* onLogin(){
     if(this.userObj.userName == "admin" && this.userObj.password == "123"){
       alert('Login successfully');
       // before redirecting to the page store credential into local storage
@@ -31,5 +37,18 @@ export class LoginComponent {
     }else{
       alert('Invalid credentials')
     }
+  }*/
+
+    // Adding code for api call login.
+  onLogin(){
+    this.http.post("https://projectapi.gerasim.in/api/UserApp/login", this.userObj).subscribe((res:any)=>{
+      if(res.result){
+      alert('Login success')
+      localStorage.setItem('loginUser', JSON.stringify(res.data));
+      this.router.navigateByUrl('user-list');
+      }else{
+        alert(res.message)
+      }
+    })
   }
 }
