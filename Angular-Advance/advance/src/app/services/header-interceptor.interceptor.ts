a4e22f57d7e8c872a22c0ee70096b95b54d38bb7
Sufiyan33 <basu700736@gmail.com>
2024-11-09 12:20:27 +0530

    Adding some other codes

--- Angular-Advance/advance/src/app/services/header-interceptor.interceptor.ts
+++ Angular-Advance/advance/src/app/services/header-interceptor.interceptor.ts
@@ -2,10 +2,8 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const headerInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
   const API_KEY = 'test123';
-  const rq = req.clone({
-    setHeaders:{
-      API_KEY,
-    }
-  })
+  const rq = req.clone()
+  rq.headers.set("SettingHeaserValue", "123")
+  rq.headers.append("SettingHeaserValue", "123")
   return next(rq);
 };
