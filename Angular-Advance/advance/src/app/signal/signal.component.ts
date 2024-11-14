bfff3e2e7baf683b6374458424c6767cdf71dfe4
Sufiyan33 <basu700736@gmail.com>
2024-11-14 11:13:29 +0530

    Testing default nature of signal

--- Angular-Advance/advance/src/app/signal/signal.component.ts
+++ Angular-Advance/advance/src/app/signal/signal.component.ts
@@ -1,20 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal',
   standalone: true,
   imports: [],
   templateUrl: './signal.component.html',
-  styleUrl: './signal.component.scss'
+  styleUrl: './signal.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignalComponent {
   fistName = signal("Angular 18")
   rollNo = signal<number>(124)
 
+  //Now lets check it changing name automatically
   constructor(){
     const fName = this.fistName();
+    setTimeout(() => {
+      this.fistName.set('Dot net')
+    }, 3000);
   }
 
+  // Not to check actuall things disable change detection
   changeName(){
     this.fistName.set("Testing signal change state")
   }
